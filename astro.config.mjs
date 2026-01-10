a16fb30253c13c64188f5a29305e770f0851fa9b
SineMunchs <167076042+SineMunchs@users.noreply.github.com>
2026-01-10 11:08:18 +0100

    commit link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from "@astrojs/tailwind";
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    vite: {
+      plugins: [tailwind()]
+    }
+  });
